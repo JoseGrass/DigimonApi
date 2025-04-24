7e9bcbfaeb128bea4a52814a52e789a661327a4d
JoseGrass <122506998+JoseGrass@users.noreply.github.com>
2025-04-24 21:45:11 +0000

    informacion

--- js/informacion.js
+++ js/informacion.js
@@ -1,4 +1,22 @@
 function mostrarInformacion(){
     const app = document.getElementById("app");
-    app.innerHTML = "Favoritos";
+    app.innerHTML = ""; // Limpiar antes de mostrar
+
+    const tarjetaInformacion = document.createElement("div");
+    tarjetaInformacion.classList.add("informacion");
+
+    tarjetaInformacion.innerHTML = `
+        <div class="overlay"></div>
+        <div class="contenido">
+            <h2>DIGIMON API</h2>
+            <h3>Jose Luis Grass Murillo</h3>
+            <img class="contenido-img" src="https://s0.smartresize.com/wallpaper/533/654/HD-wallpaper-angemo-angemon-digimon-patamon.jpg" alt="digimon">
+            <p class="caja-azul">API con información de 239 digimones</p>
+            <a href="https://github.com/JoseGrass/DigimonApi" target="_blank">
+                <p class="link">github.com/JoseGrass/DigimonApi</p>
+            </a>
+        </div>
+    `;
+
+    app.appendChild(tarjetaInformacion);
 }
