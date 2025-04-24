b95db5c314cfbcc6b29755370fa00f389da2ddad
JoseGrass <122506998+JoseGrass@users.noreply.github.com>
2025-04-24 01:03:18 +0000

    modificacion

--- js/api.js
+++ js/api.js
@@ -1,7 +1,6 @@
 async function  conexionLista() {
     const res = await fetch('https://digimon-api.vercel.app/api/digimon')
     const data = await res.json()
-    console.log(data);
     return data;
 }
 
