dacc35a10219211b3339c5976ecf616e1162cb46
JoseGrass <122506998+JoseGrass@users.noreply.github.com>
2025-05-24 23:59:35 +0000

    cambios icons

--- sw.js
+++ sw.js
@@ -2,18 +2,18 @@ self.addEventListener('install', e => {
   e.waitUntil(
     caches.open('Digimonapp-v1').then(cache => {
       return cache.addAll([
-        './', // la carpeta DigimonApi/
-        './index.html',
-        './css/styles.css',
-        './js/api.js',
-        './js/buscador.js',
-        './js/filtro.js',
-        './js/favorito.js',
-        './js/informacion.js',
-        './js/lista.js',
-        './js/original.js',
-        './icons/logo_Digi.png',
-        './icons/DigimonLogo.png'
+        '/', // la carpeta DigimonApi/
+        '/index.html',
+        '/css/styles.css',
+        '/js/api.js',
+        '/js/buscador.js',
+        '/js/filtro.js',
+        '/js/favorito.js',
+        '/js/informacion.js',
+        '/js/lista.js',
+        '/js/original.js',
+        '/icons/logo_Digi.png',
+        '/icons/DigimonLogo.png'
       ]);
     }).catch(err => {
       console.error('Error en cache.addAll:', err);
