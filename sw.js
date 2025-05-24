46b43f3a009dcee091456ad21fe06a469eb24aeb
JoseGrass <122506998+JoseGrass@users.noreply.github.com>
2025-05-24 22:49:47 +0000

    sw2

--- sw.js
+++ sw.js
@@ -1,15 +1,19 @@
-// sw.js
-
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open('Digimonapp-v1').then(cache => {
       return cache.addAll([
-        '.',                    // Página principal (index.html)
-        'index.html',
-        'css/styles.css',
-        'js/api.js',
-        'icons/logo_Digi.png',
-        'icons/DigimonLogo.png'
+        './',
+        './index.html',
+        './css/styles.css',
+        './js/api.js',
+        './js/buscador.js',
+        './js/filtro.js',
+        './js/favorito.js',
+        './js/informacion.js',
+        './js/lista.js',
+        './js/original.js',
+        './icons/logo_Digi.png',
+        './icons/DigimonLogo.png'
       ]);
     })
   );
