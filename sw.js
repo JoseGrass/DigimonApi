7670b0b773d5e6fb1a89b690274b1ff4540bcfbe
JoseGrass <122506998+JoseGrass@users.noreply.github.com>
2025-05-24 20:43:16 +0000

    service worker

---
+++ sw.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', e => {
+    e.waitUntil(
+      caches.open('pokeapp-v1').then(cache => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/css/styles.css',
+          '/js/app.js',
+          '/icons/logo_Digi.jpg',
+          '/icons/DigimonLogo.png'
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', e => {
+    e.respondWith(
+      caches.match(e.request).then(response => {
+        return response || fetch(e.request);
+      })
+    );
+  });
