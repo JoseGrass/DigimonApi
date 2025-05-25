6dc9008a1a68043b155470bcce2ace9987bf822a
JoseGrass <122506998+JoseGrass@users.noreply.github.com>
2025-05-25 04:12:32 +0000

    sw13

--- sw.js
+++ sw.js
@@ -1,22 +1,28 @@
 self.addEventListener('install', e => {
-    e.waitUntil(
-      caches.open('Digimonapp-v1').then(cache => {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/css/styles.css',
-          '/js/app.js',
-          '/icons/logo_Digi.png',
-          '/icons/DigimonLogo.png'
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', e => {
-    e.respondWith(
-      caches.match(e.request).then(response => {
-        return response || fetch(e.request);
-      })
-    );
-  });
+  e.waitUntil(
+    caches.open('Digimonapp-v1').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './css/styles.css',
+        './js/api.js',
+        './js/buscador.js',
+        './js/filtro.js',
+        './js/favorito.js',
+        './js/informacion.js',
+        './js/lista.js',
+        './js/original.js',
+        './icons/logo_Digi.png',
+        './icons/DigimonLogo.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
